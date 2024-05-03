3c74dd478397f4675e3229062f8f0495ceda2472
Myakot <cstepan@yandex.ru>
2024-05-03 22:14:06 +0700

    fixing app.py back to see what's wrong, changed index.html

--- page-analyzer/app.py
+++ page-analyzer/app.py
@@ -1,21 +1,10 @@
-from flask import (Flask, render_template, request, redirect,
-                   url_for, flash, get_flashed_messages)
-from dotenv import load_dotenv
+from flask import Flask, render_template
 import os
-from .database_manager import DbManager
-
-
-load_dotenv()
-
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
-db = DbManager()
-
-
-@app.route('/', methods=['GET'])
-def start_page():
-    messages = get_flashed_messages(with_categories=True)
-    return render_template('main.html', messages=messages)
+@app.route('/')
+def index():
+    return render_template('index.html')
