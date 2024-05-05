9342f0051b581d769c76dace671b1e194fa9d476
Myakot <cstepan@yandex.ru>
2024-05-05 17:25:26 +0700

    massive refactor

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,9 @@
 from flask import (
     Flask,
     render_template,
-    request, redirect,
+    request,
+    redirect,
     flash,
-    get_flashed_messages,
     url_for,
     abort)
 from dotenv import load_dotenv
@@ -17,6 +17,7 @@ import validators
 import requests
 
 app = Flask(__name__)
+
 load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -24,11 +25,44 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 @app.route('/')
 def index():
-    messages = get_flashed_messages(with_categories=True)
-    return render_template(
-        'index.html',
-        messages=messages
-    )
+    return render_template('index.html')
+
+
+@app.route('/urls')
+def get_urls():
+    connection = database_connect()
+    with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+        cursor.execute(
+            '''
+            SELECT DISTINCT ON (urls.id)
+                urls.id, name, url_checks.created_at, status_code
+            FROM url_checks RIGHT JOIN urls ON url_checks.url_id = urls.id
+            ORDER BY urls.id DESC, url_checks.created_at DESC;
+            '''
+        )
+        urls = cursor.fetchall()
+    connection.close()
+    return render_template('urls.html', urls=urls)
+
+
+@app.route('/urls/<int:id>')
+def get_url(id):
+    connection = database_connect()
+    with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+        cursor.execute(
+            "SELECT * FROM urls WHERE id=%s;",
+            (id, )
+        )
+        url = cursor.fetchone()
+        if not url:
+            abort(404, description='Страница не найдена', response=404)
+        cursor.execute(
+            "SELECT * FROM url_checks WHERE url_id=%s ORDER BY id DESC;",
+            (id, )
+        )
+        checks = cursor.fetchall()
+    connection.close()
+    return render_template('url.html', url=url, checks=checks)
 
 
 @app.post('/urls')
@@ -40,11 +74,14 @@ def add_url():
             flash('URL обязателен', 'danger')
         elif validators.length(url, max=255):
             flash('URL превышает 255 символов', 'danger')
-        return redirect(url_for('index'), 422)
+        return render_template('index.html', url=url), 422
     normalized_url = normalize(url)
-    connection = db_connect()
+    connection = database_connect()
     with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute("SELECT * FROM urls WHERE name=%s;", (normalized_url, ))
+        cursor.execute(
+            "SELECT * FROM urls WHERE name=%s;",
+            (normalized_url, )
+        )
         existed_url = cursor.fetchone()
         if existed_url:
             flash('Страница уже существует', 'info')
@@ -55,7 +92,8 @@ def add_url():
                 (normalized_url, datetime.datetime.now())
             )
             cursor.execute(
-                "SELECT * FROM urls WHERE name=%s;", (normalized_url,)
+                "SELECT * FROM urls WHERE name=%s;",
+                (normalized_url, )
             )
             added_url = cursor.fetchone()
             current_id = added_url.id
@@ -64,117 +102,83 @@ def add_url():
     return redirect(url_for('get_url', id=current_id), 302)
 
 
-@app.route('/urls/<int:id>')
-def get_url(id):
-    messages = get_flashed_messages(with_categories=True)
-    connection = db_connect()
-    with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute("SELECT * FROM urls WHERE id=%s;", (id, ))
-        url = cursor.fetchone()
-        if not url:
-            abort(404)
-        cursor.execute(
-            "SELECT * FROM url_checks WHERE url_id=%s ORDER BY id DESC;", (id,)
-        )
-        checks = cursor.fetchall()
-    return render_template(
-        'url.html',
-        url=url,
-        checks=checks,
-        messages=messages
-    )
-
-
-@app.route('/urls')
-def get_urls():
-    connection = db_connect()
-    with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute(
-            '''
-            SELECT DISTINCT ON (result_query.id) * FROM (
-                SELECT urls.id, name, url_checks.created_at,
-                status_code FROM url_checks
-                RIGHT JOIN urls ON url_checks.url_id = urls.id
-                ORDER BY urls.id DESC, url_checks.created_at DESC
-            ) as result_query
-            ORDER BY result_query.id DESC;
-            '''
-        )
-        all_urls = cursor.fetchall()
-    connection.close()
-    messages = get_flashed_messages(with_categories=True)
-    return render_template(
-        'urls.html',
-        urls=all_urls,
-        messages=messages
-    )
-
-
 @app.post('/urls/<int:id>/checks')
 def check_url(id):
-    connection = db_connect()
+    connection = database_connect()
     with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute("SELECT * FROM urls WHERE id=%s;", (id, ))
+        cursor.execute(
+            "SELECT * FROM urls WHERE id=%s;",
+            (id, )
+        )
         url = cursor.fetchone()
-        response = http_request(url.name)
-        if not response:
+        site_content = get_site_content(url.name)
+        if not site_content:
             flash('Произошла ошибка при проверке', 'danger')
         else:
-            page_content = get_url_content(response)
-            cursor.execute('''
-                            INSERT INTO url_checks
-                            (url_id, created_at, status_code,
-                            h1, title, description)
-                            VALUES (%s, %s, %s, %s, %s, %s);
-                            ''',
-                           (id, datetime.datetime.now(), response.status_code,
-                            page_content['h1'], page_content['title'],
-                            page_content['description']))
+            cursor.execute(
+                '''
+                INSERT INTO url_checks
+                (url_id, created_at, status_code, h1, title, description)
+                VALUES (%s, %s, %s, %s, %s, %s);
+                ''',
+                (
+                    id,
+                    datetime.datetime.now(),
+                    site_content['status_code'],
+                    site_content['h1'],
+                    site_content['title'],
+                    site_content['description']
+                )
+            )
             flash('Страница успешно проверена', 'success')
     connection.close()
     return redirect(url_for('get_url', id=id), 302)
 
 
+@app.errorhandler(503)
 @app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404_not_found.html'), 404
+def resource_not_found(error):
+    return render_template(
+        '404_not_found.html',
+        error_message=error.description
+    ), error.response
 
 
-def db_connect():
+@app.template_filter()
+def format_timestamp(datetime):
+    return datetime.strftime('%Y-%m-%d %H:%M:%S') if datetime else ''
+
+
+def database_connect():
     try:
         connection = psycopg2.connect(DATABASE_URL)
         connection.autocommit = True
         return connection
-    except requests.exceptions.ConnectionError:
-        return False
-
-
-def http_request(url):
-    try:
-        response = requests.get(url)
-        return response
-    except requests.exceptions.ConnectionError:
-        return False
-
-
-def get_url_content(response):
-    content = {
-        'h1': '',
-        'title': '',
-        'description': ''
-    }
-    page = BeautifulSoup(response.text, 'html.parser')
-    if page.find('h1'):
-        content['h1'] = page.find('h1').text
-    if page.find('title'):
-        content['title'] = page.find('title').text
-    if page.find('meta', attrs={'name': 'description'}):
-        content['description'] = page.find(
-            'meta', attrs={'name': 'description'}
-        )['content']
-    return content
+    except psycopg2.DatabaseError or psycopg2.OperationalError:
+        abort(503, description='Ошибка доступа к базе данных', response=503)
 
 
 def normalize(url):
     url = urlparse(url)
     return f'{url.scheme}://{url.netloc}'
+
+
+def get_site_content(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        page = BeautifulSoup(response.text, 'html.parser')
+        description = page.find('meta', attrs={'name': 'description'})
+        site_content = {
+            'status_code':
+                response.status_code,
+            'h1':
+                page.find('h1').text if page.find('h1') else '',
+            'title':
+                page.find('title').text if page.find('title') else '',
+            'description':
+                description['content'] if description else ''
+        }
+        return site_content
+    except requests.exceptions.RequestException:
+        return False
